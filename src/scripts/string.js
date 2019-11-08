84b1be9a6c518974ff392e4a1cb5d56a096f2920
Michael Carolin <michael.carolin@simpleviewinc.com>
2019-11-08 10:51:44 -0700

    moved delimiters in delimitString to argument

--- src/scripts/string.js
+++ src/scripts/string.js
@@ -38,11 +38,12 @@ export const snakeCase = (str) => {
  * @returns a new string with the specified delimiter delimiting each word
  * @param {String} str - string of any casing
  * @param {String} delimiter - e.g. '_'
+ * @param {Array} delimiters - optional. An array of delimiter characters on which this function searches and breaks. Defaults to checking -, _, and space
  * @example delimitString('fooBar', '_') // 'foo_Bar'
  */
-export const delimitString = (str, delimiter) => {
+export const delimitString = (str, delimiter, delimiters=['-', '_', ' ']) => {
   if (!isStr(str)) return str
-  const isDelimiter = c => ['-', '_', ' '].some(del => del === c)
+  const isDelimiter = c => delimiters.some(del => del === c)
   let prevChar = '_'
   return mapString(str, char => {
     if (isDelimiter(char)) {
