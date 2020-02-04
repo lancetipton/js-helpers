aa49332e649c1d8bd1b34e16d431f28be1668a15
Daniel Yo <dnl992004@yahoo.com.au>
2020-02-04 12:11:03 -0700

    update: - removed methods thats currently not being used in sv-keg-expo

--- src/scripts/__tests__/url.js
+++ src/scripts/__tests__/url.js
@@ -6,79 +6,77 @@ describe('/url', () => {
 
   beforeEach(() => jest.resetAllMocks())
 
-  describe('urlGetQuery', () => {
+  describe('querystringToObj', () => {
 
-    it('should return the querystring', () => {
+    it('should return a valid object with querystring items', () => {
 
-      const url = "http://www.google.com?name=daniel&id=1"
-      const querystring = Url.urlGetQuery(url)
+      const obj = Url.querystringToObj('?name=daniel&id=5')
+      expect(obj.name).toEqual('daniel')
+      expect(obj.id).toEqual('5')
 
-      expect(querystring === 'name=daniel&id=1').toBe(true)
+      const obj2 = Url.querystringToObj('????????name=daniel&id=5')
+      expect(obj2.name).toEqual('daniel')
+      expect(obj2.id).toEqual('5')
 
-      const url2 = "https://www.google.com?name=daniel"
-      const querystring2 = Url.urlGetQuery(url2)
+      const obj3 = Url.querystringToObj('/hello?name=daniel&id=5')
+      expect(obj3.name).toEqual('daniel')
+      expect(obj3.id).toEqual('5')
 
-      expect(querystring2 === 'name=daniel').toBe(true)
-
-    })
-
-    it('should return the querystring given an array querystring', () => {
-
-      const urlCommas = "http://www.google.com?color=red,green,blue"
-      const querystringCommas = Url.urlGetQuery(urlCommas)
-      expect(querystringCommas === "color=red,green,blue").toBe(true)
+      const obj4 = Url.querystringToObj('?name=some%20sentence%20with%20spaces')
+      expect(obj4.name).toEqual('some sentence with spaces')
 
     })
 
-    it('should return the querystring given special characters', () => {
+    it('should return a valid object given array', () => {
 
-      // apostrophe '
-      const url = "http://www.google.com?name=daniel'dog&id=1"
-      const querystring = Url.urlGetQuery(url)
-      expect(querystring === "name=daniel'dog&id=1").toBe(true)
+      const obj = Url.querystringToObj('?names=daniel,foo,man&id=5')
+      expect(obj.names).toEqual(expect.arrayContaining(['daniel', 'foo', 'man']))
+      expect(obj.id).toEqual('5')
 
-    })
+      const obj2 = Url.querystringToObj('?names=daniel,&id=5')
+      expect(obj2.names).toEqual(expect.arrayContaining(['daniel']))
+      expect(obj2.id).toEqual('5')
 
-    it('should ignore values that uses unencoded reserved characters', () => {
-
-      // will ignore values after : 
-      const url = "http://www.google.com?name=daniel:hello&id=1"
-      const querystring = Url.urlGetQuery(url)
-      expect(querystring === "name=daniel").toBe(true)
-
-      // will ignore values after |
-      const url2 = "http://www.google.com?name=daniel|hello&id=1"
-      const querystring2 = Url.urlGetQuery(url2)
-      expect(querystring2 === "name=daniel").toBe(true)
+      const obj3 = Url.querystringToObj('?name=daniel&groups=1%2C2%2C3')
+      expect(obj3.groups).toEqual(expect.arrayContaining(['1','2','3']))
+      expect(obj3.name).toEqual('daniel')
+      
 
     })
 
-    it('should return emptystring with invalid URL', () => {
+    it('should return the last set of valid querystring items', () => {
 
-      const urls = [
-        "not a url?name=danielk",
-        "?name=daniel"
-      ]
+      const obj = Url.querystringToObj('?name=daniel&id=5^^^*^*^*^*^*^foo=bar')
+      expect(obj.name).toEqual('daniel')
+      expect(obj.id).toEqual('5')
+      expect (obj.foo).toEqual(undefined)
 
-      urls.map((url) => {
-        expect(Url.urlGetQuery(url) == "").toBe(true)
-      })
+      const obj2 = Url.querystringToObj('?color=foobar???name=daniel&id=5')
+      expect(obj2.name).toEqual('daniel')
+      expect(obj2.id).toEqual('5')
+      expect (obj.color).toEqual(undefined)
 
     })
 
-    it('should return emptystring if querystring not found', () => {
+    it('should return empty object on invalid querystring', () => {
 
-      const urls = [
-        "www.google.com?",
-        "www.google.com/user/id"
-      ]
+      const obj = Url.querystringToObj('just some random string?hello')
+      expect(obj).toEqual({})
 
-      urls.map((url) => {
-        expect(Url.urlGetQuery(url) == "").toBe(true)
-      })
+      const obj3 = Url.querystringToObj('just some random string')
+      expect(obj3).toEqual({})
+
     })
 
-        
+    it('should combine duplicate keys into array', () => {
+
+      const obj = Url.querystringToObj('?names=daniel&names=foo')
+      expect(obj.names).toEqual(expect.arrayContaining(['daniel', 'foo']))
+
+      const obj2 = Url.querystringToObj('?names=&names=foo')
+      expect(obj2.names).toEqual(expect.arrayContaining(['foo', '']))
+
+    })
   })
 
   describe('isValidUrl', () => {
@@ -114,286 +112,7 @@ describe('/url', () => {
         
   })
 
-  describe('getUrlQueryObj', () => {
-
-    it('should return emptystring with invalid URL', () => {
-
-      const urls = [
-        "not a url?name=danielk",
-        "?name=daniel"
-      ]
-
-      urls.map((url) => {
-        expect(Url.getUrlQueryObj(url) == "").toBe(true)
-      })
-
-    })
-
-    it('should return object mapping with array', () => {
-
-      const url = "https://google.com?name=daniel&id=1,2,3,4"
-      const obj = Url.getUrlQueryObj(url)
-      expect(obj.name).toEqual('daniel')
-      expect(obj.id).toEqual(['1', '2', '3', '4'])
-
-    })
-
-    it('should return object mapping prior to the reserved colon char', () => {
-
-      const url = "https://google.com?name=daniel&id=5:2&animal=foo"
-      const obj = Url.getUrlQueryObj(url)
-      expect(obj.name).toEqual('daniel')
-      expect(obj.id).toEqual('5')
-      expect(obj.animal).toEqual(undefined)
-
-    })
-
-    it('should return object mapping prior to the reserved pipe char', () => {
-
-      const url = "https://google.com?name=daniel&id=5|2&animal=foo"
-      const obj = Url.getUrlQueryObj(url)
-      expect(obj.name).toEqual('daniel')
-      expect(obj.id).toEqual('5')
-      expect(obj.animal).toEqual(undefined)
-
-    })
-
-    it('should return a valid object with querystring items', () => {
-
-      const url = "https://google.com?name=daniel&id=1"
-      const obj = Url.getUrlQueryObj(url)
-      expect(obj.name).toEqual('daniel')
-      expect(obj.id).toEqual('1')
-
-      const url2 = "https://google.com???????test=daniel&&id=5"
-      const obj2 = Url.getUrlQueryObj(url2)
-      expect(obj2.test).toEqual('daniel')
-      expect(obj2.id).toEqual('5')
-    })
-
-    it('should return an empty object from url without querystring', () => {
-
-      const url = "https://google.com?"
-      const obj = Url.getUrlQueryObj(url)
-      expect(obj).toEqual({})
-
-      const url2 = "https://google.com?name"
-      const obj2 = Url.getUrlQueryObj(url2)
-      expect(obj2).toEqual({})
-
-    })
-    
-  })
-
-  describe('urlHasQueryKey', () => {
-
-    it('should return FALSE on a matched URL while using any reserved char ', () => {
-
-      const url = "https://google.com?nam:e=danielk"
-      expect(Url.urlHasQueryKey(url, 'nam:e')).toBe(false)
-
-      const urlPipe = "https://google.com?nam|e=danielk"
-      expect(Url.urlHasQueryKey(urlPipe, 'nam|e')).toBe(false)
-
-      const urlComma = "https://google.com?nam,e=danielk"
-      expect(Url.urlHasQueryKey(urlComma, 'nam,e')).toBe(false)
-
-    })
-
-    it('should return FALSE on a invalid URL', () => {
-
-      const urls = [
-        "not a url?name=danielk",
-        "?name=daniel"
-      ]
-
-      urls.map((url) => {
-        expect(Url.urlHasQueryKey(url, 'name')).toBe(false)
-      })
-
-    })
-
-    it('should return TRUE on a found querystring key', () => {
-
-      const url = "https://google.com?name=daniel&id=1"
-      const obj = Url.urlHasQueryKey(url, 'name')
-      expect(obj).toBeTruthy()
-
-      const obj2 = Url.urlHasQueryKey(url, 'id')
-      expect(obj2).toBeTruthy()
-
-    })
-
-    it('should return FALSE with invalid url or nonexistent query key', () => {
-
-      const url1 = "https://google.com?fakequerystring"
-      const obj1 = Url.urlHasQueryKey(url1, 'fakequerystring')
-      expect(obj1).toBeFalsy()
-
-      const url2 = "https://google.com/user/help=2"
-      const obj2 = Url.urlHasQueryKey(url2, 'help')
-      expect(obj2).toBeFalsy()
-
-    })
-    
-  })
-
-  describe('getUrlObj', () => {
-
-    it('should return with Full object values', () => {
-
-      const url = "https://google.com:8080/path/yo?name=daniel&id=1#some-hash"
-      const obj = Url.getUrlObj(url)
-      expect(obj).toHaveProperty('url', url)
-      expect(obj).toHaveProperty('protocol', 'https')
-      expect(obj).toHaveProperty('slash', '://')
-      expect(obj).toHaveProperty('host', 'google.com')
-      expect(obj).toHaveProperty('port', ':8080')
-      expect(obj).toHaveProperty('path', '/path/yo')
-      expect(obj).toHaveProperty('query', 'name=daniel&id=1')
-      expect(obj).toHaveProperty('hash', 'some-hash')
-
-    })
-
-    it('should return with Some object values', () => {
-
-      const url = "http://www.google.com:8080/?name=daniel&id=1"
-      const obj = Url.getUrlObj(url)
-      expect(obj).toHaveProperty('url', url)
-      expect(obj).toHaveProperty('host', 'www.google.com')
-      expect(obj).toHaveProperty('port', ':8080')
-      expect(obj).toHaveProperty('query', 'name=daniel&id=1')
-
-    })
-
-    it('should return with no object values', () => {
-
-      const url = "not a valid url input"
-      expect(Url.getUrlObj(url)).toEqual({})
-
-    })
-  })
-
-  describe('urlAddQuerystring', () => {
-
-    it('should return the URL with querystring given a param object', () => {
-
-      const url = "https://google.com"
-      const param = {
-        name: 'daniel',
-        food: 'pasta'
-      }
-      const obj = Url.urlAddQuerystring(url, param)
-      expect(obj).toEqual('https://google.com?name=daniel&food=pasta')
-
-
-      const url2 = "https://google.com?id=500"
-      const param1 = {
-        name: 'daniel',
-        food: 'pasta'
-      }
-      const obj2 = Url.urlAddQuerystring(url2, param1)
-      expect(obj2).toEqual('https://google.com?id=500&name=daniel&food=pasta')
-
-    })
-
-    it('should return the URL with querystring given a param string', () => {
-
-      const url = "https://google.com"
-      const param = 'test=daniel&id=123'
-      const obj = Url.urlAddQuerystring(url, param)
-      expect(obj).toEqual('https://google.com?test=daniel&id=123')
-
-
-      const url2 = "https://google.com?id=500"
-      const param1 = 'animal=cat&color=red'
-      const obj2 = Url.urlAddQuerystring(url2, param1)
-      expect(obj2).toEqual('https://google.com?id=500&animal=cat&color=red')
-
-    })
-
-    it('should return the URL if param object is not valid', () => {
-
-      const url = "https://google.com"
-      const param = {
-        foo: () => {},
-        bar: {}
-      }
-      const obj = Url.urlAddQuerystring(url, param)
-      expect(obj).toEqual('https://google.com')
-
-    })
-
-    it('should return emptystring when no url param', () => {
-
-      const param = {}
-      const obj = Url.urlAddQuerystring(null, param)
-      expect(obj).toEqual('')
-
-      const param2 = {
-        test: 'foo'
-      }
-      const obj2 = Url.urlAddQuerystring(null, param2)
-      expect(obj2).toEqual('')
-
-    })
-
-  })
-
-  describe('urlUpsertQuerystring', () => {
-
-    it('should return the URL with added querystring (no existing querystring)', () => {
-
-      const url = "https://google.com"
-      const obj = Url.urlUpsertQuerystring(url, 'id', 5)
-      expect(obj).toEqual('https://google.com?id=5')
-
-    })
-
-    it('should return the URL with added querystring (existing querystring)', () => {
-
-      const url = "https://google.com?name=daniel"
-      const obj = Url.urlUpsertQuerystring(url, 'id', 5)
-      expect(obj).toEqual('https://google.com?name=daniel&id=5')
-
-    })
-
-    it('should replace the existing querystring key', () => {
-
-      // replace key at the beginning
-      const url = "https://google.com?name=daniel"
-      const obj = Url.urlUpsertQuerystring(url, 'name', 'foooooo')
-      expect(obj).toEqual('https://google.com?name=foooooo')
-
-      // replace key in the middle
-      const url2 = "https://google.com?name=daniel&id=100&color=red"
-      const obj2 = Url.urlUpsertQuerystring(url2, 'id', 50)
-      expect(obj2).toEqual('https://google.com?name=daniel&id=50&color=red')
-
-      // replace key at the end
-      const url3 = "https://google.com?name=daniel&id=100&color=red"
-      const obj3 = Url.urlUpsertQuerystring(url3, 'color', 'yellow')
-      expect(obj3).toEqual('https://google.com?name=daniel&id=100&color=yellow')
-
-    })
-
-    it('should return emptystring when no/invalid url provided', () => {
-
-      const obj = Url.urlUpsertQuerystring(null, 'name', 'foooooo')
-      expect(obj).toEqual('')
-
-      const obj2 = Url.urlUpsertQuerystring('null', 'name', 'foooooo')
-      expect(obj2).toEqual('')
-
-      // invalid url
-      const obj3 = Url.urlUpsertQuerystring('google.com', 'name', 'foooooo')
-      expect(obj3).toEqual('')
-
-    })
-
-  })
-
-  describe('objToUrlQuerystring', () => {
+  describe('objToQuerystring', () => {
 
     it('return a valid querystring from the given object with strings', () => {
 
@@ -401,8 +120,14 @@ describe('/url', () => {
         name: 'daniel',
         food: 'pasta'
       }
-      const result = Url.objToUrlQuerystring(obj)
-      expect(result).toEqual('name=daniel&food=pasta')
+      const result = Url.objToQuerystring(obj)
+      expect(result).toEqual('?name=daniel&food=pasta')
+
+      const obj2 = {
+        name: 'some sentence with spaces',
+      }
+      const result2 = Url.objToQuerystring(obj2)
+      expect(result2).toEqual('?name=some%20sentence%20with%20spaces')
 
     })
 
@@ -412,8 +137,8 @@ describe('/url', () => {
         name: 'daniel',
         id: 100
       }
-      const result = Url.objToUrlQuerystring(obj)
-      expect(result).toEqual('name=daniel&id=100')
+      const result = Url.objToQuerystring(obj)
+      expect(result).toEqual('?name=daniel&id=100')
 
     })
 
@@ -425,9 +150,9 @@ describe('/url', () => {
           foo: 'bar'
         }
       }
-      const result = Url.objToUrlQuerystring(obj)
+      const result = Url.objToQuerystring(obj)
       // just appends the nested object via JSON string
-      expect(result).toEqual('name=daniel&id=%7B%22foo%22%3A%22bar%22%7D')
+      expect(result).toEqual('?name=daniel&id=%7B%22foo%22%3A%22bar%22%7D')
 
     })
 
@@ -437,21 +162,20 @@ describe('/url', () => {
         name: 'daniel',
         alive: true
       }
-      const result = Url.objToUrlQuerystring(obj)
+      const result = Url.objToQuerystring(obj)
       // just appends the nested object via JSON string
-      expect(result).toEqual('name=daniel&alive=true')
+      expect(result).toEqual('?name=daniel&alive=true')
 
     })
 
-    it('return a valid querystring from the given object with array', () => {
+    it('should convert array object for commas', () => {
 
       const obj = {
         name: 'daniel',
         groups: [1, 2, 3]
       }
-      const result = Url.objToUrlQuerystring(obj)
-      // just appends the nested object via JSON string
-      expect(result).toEqual('name=daniel&groups=%5B1%2C2%2C3%5D')
+      const result = Url.objToQuerystring(obj)
+      expect(result).toEqual('?name=daniel&groups=1%2C2%2C3')
 
     })
 
@@ -461,44 +185,17 @@ describe('/url', () => {
         name: 'daniel',
         func: () => {}
       }
-      const result = Url.objToUrlQuerystring(obj)
-      expect(result).toEqual('name=daniel')
+      const result = Url.objToQuerystring(obj)
+      expect(result).toEqual('?name=daniel')
 
     })
 
     it('should return emptystring on null or empty obj', () => {
 
-      expect(Url.objToUrlQuerystring({})).toEqual('')
-      expect(Url.objToUrlQuerystring(null)).toEqual('')
+      expect(Url.objToQuerystring({})).toEqual('')
+      expect(Url.objToQuerystring(null)).toEqual('')
 
     })
 
-  })
-
-  describe('querystringToObj', () => {
-
-    it('should return a valid object with querystring items', () => {
-
-      const obj = Url.querystringToObj('?name=daniel&id=5')
-      expect(obj.name).toEqual('daniel')
-      expect(obj.id).toEqual('5')
-
-      const obj2 = Url.querystringToObj('????????name=daniel&id=5')
-      expect(obj2.name).toEqual('daniel')
-      expect(obj2.id).toEqual('5')
-
-      const obj3 = Url.querystringToObj('/hello?name=daniel&id=5')
-      expect(obj3.name).toEqual('daniel')
-      expect(obj3.id).toEqual('5')
-
-    })
-
-    it('should return empty object on invalid querystring', () => {
-
-      const obj3 = Url.querystringToObj('just some random string')
-      expect(obj3).toEqual({})
-
-    })
-   
   })
 })
--- src/scripts/url.js
+++ src/scripts/url.js
@@ -7,21 +7,10 @@ import { isStr } from './string'
 import { isNum } from './number'
 import { isBool } from './boolean'
 import { isColl } from './collection'
+import { isArr } from './array'
 
-/**
- * Turns a given url into an object of querystring items
- * @param {String} url - url to pull the query params from
- * 
- * @returns {Object} 
- */
-export const getUrlQueryObj = url => {
-
-  // verify url
-  if (!isValidUrl(url)) return ''
-
-  const params = urlGetQuery(url)
-  return querystringToObj(params)
-}
+// 1. takes a querystring and return object
+// 2. takes an object and return querystring
 
 /**
  * takes a raw querystring input and converts it to an object
@@ -41,16 +30,27 @@ export const querystringToObj = querystring => {
   querystring = /(\?*)([a-zA-Z0-9\=\&\%\$\-\_\.\+\!\*\'\(\)\,]+)/.exec(querystring)[2]
 
   const split = querystring.split('&')
+
   split.length &&
     split.map(item => {
       const itemSplit = item.split('=')
+
       if (itemSplit.length === 2) {
         // if the value contains special char ',' then make it into an array
-        const array = itemSplit[1].split(',')
+        const array = decodeURIComponent(itemSplit[1]).split(',')
         if (array && array.length > 1)
-          currentQueryItems[decodeURIComponent(itemSplit[0])] = array
+          currentQueryItems[itemSplit[0]] = array
+        
+        // check if key already exists
+        else if (itemSplit[0] in currentQueryItems) {
+           // convert to array or append to it
+           const val = currentQueryItems[itemSplit[0]]
+           currentQueryItems[itemSplit[0]] = isArr(val) 
+             ? val.push(decodeURIComponent(itemSplit[1])) 
+             : [val, decodeURIComponent(itemSplit[1])]        
+        }
         else
-          currentQueryItems[decodeURIComponent(itemSplit[0])] = decodeURIComponent(itemSplit[1])
+          currentQueryItems[itemSplit[0]] = decodeURIComponent(itemSplit[1])
       }
     })
 
@@ -61,155 +61,30 @@ export const querystringToObj = querystring => {
  * Converts the input object to url querystring
  * @param {Object} obj - object with kvp to convert into a querystring
  * 
- * @returns {String} url querystring
+ * @returns {String} querystring
  */
-export const objToUrlQuerystring = obj => {
+export const objToQuerystring = obj => {
   let firstSet
   return reduceObj(obj, (key, value, urlStr) => {
     if(!value) return urlStr
 
     const useVal = isStr(value) || isNum(value) || isBool(value)
       ? value 
-      : isObj(value) || isColl(value)
-        ? JSON.stringify(value) 
+      : isColl(value)
+        ? isArr(value) 
+          ? value.join(',') 
+          : JSON.stringify(value)
         : null
     
     if(!useVal) return urlStr
 
     urlStr = !firstSet
-      ? `${encodeURIComponent(key)}=${encodeURIComponent(useVal)}`
+      ? `?${encodeURIComponent(key)}=${encodeURIComponent(useVal)}`
       : `${urlStr}&${encodeURIComponent(key)}=${encodeURIComponent(useVal)}`
     firstSet = true
 
     return urlStr
   }, '')
-}
-
-/**
- * Parse the given url to a url object
- * @param {String} url - complete url. i.e 'https://google.com/stuff'
- * 
- * @returns {Object} {url, protocol, slash, host, port, path, query, hash}
- */
-export const getUrlObj = url => {
-  const urlRegEx = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-  const result = urlRegEx.exec(url || window.location.href)
-  const urlData = {}
-
-  // return empty on possible invalid input
-  if (!result)
-    return urlData
-  
-  const keys = ['url', 'protocol', 'slash', 'host', 'port', 'path', 'query', 'hash']
-
-  // map each keys to the regex result values
-  keys.forEach((key, i) => {
-
-    typeof result[i] === "undefined" 
-      ? urlData[key] = "" 
-        // append special character infront of 'port' and 'slash' values
-      : (key === "port" || key === "slash") 
-        ? urlData[key] = `:${result[i]}`
-        : urlData[key] = result[i]
-
-  })
-
-  urlData['path'] = "/" + urlData['path']
-  return urlData
-}
-
-/**
- * create or update the url with the kvp
- * @param {String} url - complete url you want to upsert to
- * @param {String} key - key name of your query item
- * @param {String|Number} value - value of your query item
- * 
- * @returns {String} updated querystring url
- */
-export const urlUpsertQuerystring = (url, key, value) => {
-
-  // verify url
-  if (!isValidUrl(url)) return ''
-
-  const regex = new RegExp('(\/?|\/&)'+ key +'=[^&]*')
-  const param = `${key}=${encodeURIComponent(value)}`
-
-  // either replace or add
-  return regex.test(url)
-    ? url.replace(regex, `${param}`)
-    : urlAddQuerystring(url, param)
-}
-
-/**
- * Adds a querystring to a given url. pass in either an object of kvp or a string of the full querystring
- * @param {String} url - complete url you want to add querystring items to
- * @param {String|Object} param - can be the raw querystring or kvp
- * 
- * @returns {String} - url with added querystring
- */
-export const urlAddQuerystring = (url, param) => {
-
-  // verify url
-  if (!isValidUrl(url)) return ''
-
-  let querystring = ''
-  if (isObj(param)) {
-    Object.entries(param).forEach(([key, value], index) => {
-
-      // validate that value is a string or a number, use reduce
-      if (!isStr(value) && !isNum(value)) return 
-
-      querystring += `${key}=${encodeURIComponent(value)}`
-      // append '&' if there's more keys to add in
-      if (index < Object.keys(param).length - 1) querystring += '&'
-    })
-
-  }
-  else if (isStr(param)) {
-    querystring = param
-  }
-
-  if (querystring !== '') {
-    url.indexOf('?') === -1 && (url += '?')
-    url.indexOf('=') !== -1 && (url += '&')
-  }
-
-  return `${url}${querystring}`
-}
-
-/**
- * Checks whether a given querystring key exists in the url
- * @param {String} url - complete url that you want to check against
- * @param {String} key - key to be checked against the url
- * 
- * @returns {Boolean}
- */
-export const urlHasQueryKey = (url, key) => {
-
-  // verify url
-  if (!isValidUrl(url)) return false
-
-  // if a key contains any of the reserved chars, it messes up regex and we shouldnt support the char
-  const hasReservedChar = /[ |;\/?:@&=+$,]/.test(key)
-  if (hasReservedChar) return false
-  
-  const regex = new RegExp('(\&)*(' + key + '=)', 'g')
-  return regex.test(urlGetQuery(url))
-}
-
-/**
- * Gets the raw querystring excluding the first '?'
- * @param {String} url - url string  i.e 'https://zerista.com?id=5'
- * 
- * @returns {String} raw querystring only. i.e: 'name=daniel&id=1'
- */
-export const urlGetQuery = url => {
-  // verify url
-  if (!isValidUrl(url)) return ''
-
-  const queryString = /\?[a-zA-Z0-9\=\&\%\$\-\_\.\+\!\*\'\(\)\,]+/.exec(url)
-
-  return queryString ? decodeURIComponent(queryString[0].replace(/\+/g,' ').replace('?','')) : ''
 }
 
 /**
