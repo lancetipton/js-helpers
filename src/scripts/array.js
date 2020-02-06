ad5c301703cb80dfc3269ca1445e438124722c92
Michael Carolin <michael.carolin@simpleviewinc.com>
2020-02-06 12:30:43 -0700

    added eitherArr

--- src/scripts/array.js
+++ src/scripts/array.js
@@ -138,3 +138,15 @@ export const flatMap = (arr, mapFn) => {
   )
 }
 
+/**
+ * Returns a if it is an Array, else returns b. Remove once added to jsutils
+ * @function
+ * @param {*} a 
+ * @param {*} b 
+ * @returns {*} either a, if it's an array, or b
+ * 
+ * @example
+ * const foo = eitherArr('hi', 1) // returns 1
+ * const bar = eitherArr([ 2 ], 1) // returns [ 2 ]
+ */
+export const eitherArr = (a, b) => isArr(a) ? a : b
